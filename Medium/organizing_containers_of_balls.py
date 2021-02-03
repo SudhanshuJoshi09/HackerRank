88149febc3f0dae890993cd1241a0ce1a2e6fefd
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2021-02-03 22:03:54 +0530

    e ll

---
+++ Medium/organizing_containers_of_balls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main():
+    q = int(input())
+    for i in range(q):
+        n = int(input())
+        arr = []
+        for j in range(n):
+            lst = list(map(int, input().split()))
+            arr.append(lst)
+        
+        
+
+
+if __name__ == '__main__':
+    main()
