c0279e0a727d24ee427953cd4ea1e1b410f86b04
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2021-02-04 13:29:01 +0530

    Tree/easy

---
+++ python/Data-Structures/Tree/easy/level-order-traversal.py
@@ -0,0 +1,76 @@
+## Level Order Traversal is Breadth First Traversal for tree
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+# Level Order Traversal is breadth first traversal for tree
+q = []
+def levelOrder(root):
+    global q
+    
+    left = root.left
+    right = root.right
+    
+    if left is not None:
+        q.append(left)
+        
+    if right is not None:
+        q.append(right)
+    
+    print(root.info, end = ' ')
+    
+    if not q:
+        return None
+    
+    n = q.pop(0)
+    levelOrder(n)
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
